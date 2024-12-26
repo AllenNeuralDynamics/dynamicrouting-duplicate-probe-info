9ace4f57cdeef6a1618fc9b475eb7da51d9c61ad
Ben Hardcastle <ben.hardcastle@alleninstitute.org>
2024-12-26 23:34:40 +0000

    Remove devices from oebin with no data on S3

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1,5 +1,10 @@
+from __future__ import annotations
+
 import concurrent.futures
 import logging
+import json
+import pathlib
+from typing import Iterable
 
 import aind_session
 import npc_session
@@ -18,6 +23,7 @@ logger = logging.getLogger(__name__)
 tracked = upath.UPath(
         "https://raw.githubusercontent.com/AllenInstitute/npc_lims/refs/heads/main/tracked_sessions.yaml"
     ).read_text()
+
 
 def get_info(session_id: str) -> list[dict[str, str]]:
     """Read the session's zarr ecephys data on S3 and find the probes included"""
@@ -44,9 +50,9 @@ def get_info(session_id: str) -> list[dict[str, str]]:
             continue
         info.append(
             dict(
-                session_id=session.id,
+                session_id=str(session.id),
                 path=zarr_path.as_posix(),
-                probe=probe,
+                probe=str(probe),
                 band=band,
             )
         )
@@ -181,9 +187,7 @@ def add_oebin_files_to_modify(df: pl.DataFrame) -> pl.DataFrame:
         assert row['to_delete'], f"No files to delete: {row}"
         p = upath.UPath(next(p for p in row['to_delete'] if 'continuous' in p))
         recording = next(d for d in p.parents if d.name == 'continuous').parent
-        paths = list(recording.glob('*.oebin'))
-        assert len(paths) == 1, f"Expected one file, got: {paths}"
-        to_modify.append(paths[0].as_posix())
+        to_modify.extend([p.as_posix() for p in recording.glob('*.oebin')])
     return df.hstack(
         [pl.Series('to_modify', to_modify)]
     ) 
@@ -210,14 +214,102 @@ def cast_lists_to_strings(df: pl.DataFrame) -> pl.DataFrame:
             )
     return df
 
+def remove_devices_from_oebin(contents: dict, device_names: Iterable[str]) -> dict:
+    if not device_names:
+        raise ValueError('device_names must not be empty')
+    any_deleted = False
+    for name in device_names:
+        assert name.endswith('AP') or name.endswith('LFP')
+        assert name.split('-')[0][0] in 'ABCDEF'
+        for subdir_name in ('events', 'continuous'):    
+            # iterate over copy of list so as to not disrupt iteration when elements are removed
+            for device_info in (d for d in contents[subdir_name]):
+                stream_name = device_info['stream_name']
+                if stream.endswith(name):
+                    logger.debug(f'Removing {subdir_name}/{stream_name} from structure.oebin')
+                    contents[subdir_name].remove(device_info)
+                    any_deleted = True
+    assert any_deleted, "No devices removed from oebin contents"
+    return contents
+
+
+def remove_devices_from_oebin(contents: dict, device_names: Iterable[str]) -> dict:
+    if not device_names:
+        raise ValueError('device_names must not be empty')
+    any_deleted = False
+    for name in device_names:
+        assert name.endswith('AP') or name.endswith('LFP')
+        assert name.split('-')[0][0] in 'ABCDEF'
+        for subdir_name in ('events', 'continuous'):    
+            # iterate over copy of list so as to not disrupt iteration when elements are removed
+            for device_info in (d for d in contents[subdir_name]):
+                stream = device_info['stream_name']
+                if stream.endswith(name):
+                    logger.debug(f'Removing {subdir_name}/{stream} from structure.oebin')
+                    contents[subdir_name].remove(device_info)
+                    any_deleted = True
+    assert any_deleted, "No devices removed from oebin contents"
+    return contents
+
+def remove_missing_from_oebin(oebin_path: str | upath.UPath) -> dict:
+    oebin_path = upath.UPath(oebin_path)
+    contents = json.loads(oebin_path.read_text())
+    for subdir_name in ('events', 'continuous'):    
+        for device_info in (d for d in contents[subdir_name]):
+            # iterate over copy of list so as to not disrupt iteration when elements are removed
+            
+            stream_name = device_info['stream_name']
+            folder_name = device_info['folder_name']
+            if "MessageCenter" in folder_name:
+                continue
+            clipped_dir = oebin_path.parent / subdir_name / folder_name
+            logger.debug(f"Checking {clipped_dir} exists and is not empty")
+            if not clipped_dir.exists():
+                logger.info(f'Removing {subdir_name}/{stream_name} from structure.oebin as {clipped_dir} does not exist on S3')
+                contents[subdir_name].remove(device_info)
+            elif not (paths := next(clipped_dir.rglob('*'), None) or tuple(clipped_dir.iterdir())):
+                logger.info(f'Removing {subdir_name}/{stream_name} from structure.oebin as {clipped_dir} is empty on S3')
+                contents[subdir_name].remove(device_info)
+            else:
+                logger.debug(f"Found at least {len(paths)} paths in {clipped_dir} - leaving info in oebin file")
+            
+            if subdir_name == 'events':
+                continue # duplicate device info in events doesn't cause a problem at this stage, but may be removed at a later stage 
+            experiment = oebin_path.parent.parent.name
+            node = oebin_path.parent.parent.parent.name
+            # currently-used version of upath (pre v2?) cannot handle paths with '#'
+            compressed_dir = oebin_path.parent.parent.parent.parent.parent / "ecephys_compressed"
+            zarr_name = f"{experiment}_{node}#{folder_name.replace('/', '').replace('TTL', '')}.zarr"
+            _zarr_path = f"{compressed_dir}/{zarr_name}"
+            logger.debug(f"Checking {_zarr_path} exists")
+            if not next(compressed_dir.glob(zarr_name), None):
+                logger.warning(f'Removing {subdir_name}/{stream_name} from structure.oebin as {_zarr_path} does not exist: paths available {[p.name for p in compressed_dir.iterdir()]}')
+    return contents
+
 if __name__ == "__main__":
-    df = (
-        get_probe_info_all_sessions()
-        .pipe(get_duplicates_to_delete)
-        .pipe(filter_dr_sessions)
-        .pipe(add_clipped_paths_to_delete)
-        .pipe(add_oebin_files_to_modify)
-        .pipe(simplify)
-    )
-    df.write_parquet('/results/duplicate_probes.parquet')
-    df.pipe(cast_lists_to_strings).write_csv('/results/duplicate_probes.csv')
+    test = False
+
+    if test:
+        logger.warning(f"Test mode: reading .parquet from /code")
+        df = pl.read_parquet('/code/duplicate_probes.parquet')
+    else:
+        df = (
+            get_probe_info_all_sessions()
+            .pipe(get_duplicates_to_delete)
+            .pipe(filter_dr_sessions)
+            .pipe(add_clipped_paths_to_delete)
+            .pipe(add_oebin_files_to_modify)
+            .pipe(simplify)
+        )
+
+    for row in df.iter_rows(named=True):
+        orig_path = row['to_modify']
+        original = json.loads(upath.UPath(orig_path).read_text())
+        updated = remove_missing_from_oebin(orig_path)
+        new = remove_devices_from_oebin(updated, row['names'])
+        new_path = upath.UPath(orig_path.replace('s3:/', '/results/to_replace'))
+        new_path.parent.mkdir(parents=True, exist_ok=True)
+        new_path.write_text(json.dumps(new, indent=4))
+    df.rename({'to_modify': 'to_replace'})
+    df.write_parquet('/results/to_delete.parquet')
+    df.pipe(cast_lists_to_strings).write_csv('/results/to_delete.csv')
